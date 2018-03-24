726e50d06b439e76ab65be6046cb5553f075c577
nnh <7bmzkr7h@sina.com>
2018-03-24 20:30:55 +0800

    1.延长休息 2.port配置化 3.页面地址修改

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const log = require('./lib/log');
 const app = require('express')();
 const http = require('http').Server(app);
+const { port } = require('./config');
 const aggregator = new (require('./lib/aggregator'))(require('socket.io')(http));
 
-app.get('/coin', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/huobi', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-http.listen(26002, () => log.info('listening on 26002'));
+http.listen(port, () => log.info(`listening on ${port}`));
